7aebe548d64e65ac678ff82faed2bdab30cc4d47
David Appelhans <dappelh@us.ibm.com>
2021-03-16 16:32:07 -0400

    serial fortran matvec

--- ex1-matvec/fortran/main.F90
+++ ex1-matvec/fortran/main.F90
@@ -2,12 +2,20 @@ program main
   use matvec
   use nvtx_mod
   implicit none
+  integer :: n
+  real(kind=8), allocatable :: A(:), x(:), y(:)
+
+  n = 10
+  allocate( A(n*n), x(n), y(n) )
+
 
   print *, "hello from main"
 
-  call matvec_hello()
+  call matvec_serial(A,x,y,n)
 
+  write(*,'(F8.4)') y(:)
 
+  return
 
 
 end program main
--- ex1-matvec/fortran/matvec.F90
+++ ex1-matvec/fortran/matvec.F90
@@ -2,16 +2,20 @@ module matvec
 
 
 contains
-
-  subroutine matvec_hello()
-
-    print *, "hello from module"
-  end subroutine matvec_hello
-
   
-  subroutine matvec_serial()
+  subroutine matvec_serial(A,x,y,n)
+    integer, intent(in) :: n
+    real(kind=8), intent(in) :: A(n*n)
+    real(kind=8), intent(in) :: x(n)
+    real(kind=8), intent(out) :: y(n)
     
-
+    ! Serial version of matrix vector multiplication. 
+    ! y = A*x
+    do j = 1 , n ! rows
+       do i = 1, n ! columns
+          y(j) = A(i + (j-1)*n) * x(j)
+       enddo
+    enddo
 
   end subroutine matvec_serial
 
