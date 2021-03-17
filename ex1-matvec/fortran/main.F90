62aae97c296b633ec3638777b0076e220f44309f
David Appelhans <dappelh@us.ibm.com>
2021-03-17 11:21:55 -0400

    v1 and v2(shared mem) fixed up and working

--- ex1-matvec/fortran/main.F90
+++ ex1-matvec/fortran/main.F90
@@ -2,18 +2,57 @@ program main
   use matvec
   use nvtx_mod
   implicit none
-  integer :: n
-  real(kind=8), allocatable :: A(:), x(:), y(:)
 
-  n = 10
+  integer(mINT) :: n
+  real(kind=8), allocatable, pinned :: A(:), x(:), y(:)
+  real(kind=8), allocatable, device :: A_d(:), x_d(:), y_d(:)
+  integer :: istat
+  CHARACTER(len=32) :: arg
+  
+  ! get n from as a runtime arguement
+  
+  call get_command_argument(1, VALUE=arg, STATUS=istat)
+  if (istat==0) then 
+     read(arg,*), n
+  else
+     n = 20*1024
+  endif
+
+  print *, "n = ", n
+
   allocate( A(n*n), x(n), y(n) )
 
+  A(:) = 1.0d+0
+  x(:) = 1.0d+0
 
   print *, "hello from main"
 
+  call nvtxStartRange("serial", color=1)
   call matvec_serial(A,x,y,n)
+  call nvtxEndRange()
 
-  write(*,'(F8.4)') y(:)
+  ! if A and x are 1, y is n.
+  !write(*,'(F8.4)') y(:)
+  
+  ! Make device versions of arrays and populate with same data as x and A
+  !  allocate( A_d(n), x_d(n), y_d(n) )
+  allocate( A_d, SOURCE=A)
+  allocate( x_d, SOURCE=x)
+  allocate( y_d, MOLD=y)
+  
+
+  ! Do niave v1 with blocks/threads over loop over rows.
+  call matvec_cudaV1(A_d,x_d,y_d,n)
+
+  y_d = y
+  write(*,'(F14.4)') y(2)
+
+  ! Do shared memory batched loading of A and x.
+  call matvec_cudaV2(A_d,x_d,y_d,n)
+
+  y_d = y
+  write(*,'(F14.4)') y(2)
+
 
   return
 
--- ex1-matvec/fortran/matvec.F90
+++ ex1-matvec/fortran/matvec.F90
@@ -1,23 +1,135 @@
 module matvec
-
+  use cudafor
+  integer, parameter :: mINT=8
 
 contains
   
   subroutine matvec_serial(A,x,y,n)
-    integer, intent(in) :: n
+    implicit none
+    integer(mINT), intent(in) :: n
     real(kind=8), intent(in) :: A(n*n)
     real(kind=8), intent(in) :: x(n)
     real(kind=8), intent(out) :: y(n)
+
+    ! local variables
+    integer :: i, j
     
     ! Serial version of matrix vector multiplication. 
     ! y = A*x
     do j = 1 , n ! rows
+       y(j) = 0.0d+0
        do i = 1, n ! columns
-          y(j) = A(i + (j-1)*n) * x(j)
+          y(j) = y(j) + A(i + (j-1)*n) * x(i)
        enddo
     enddo
 
   end subroutine matvec_serial
+
+
+  subroutine matvec_cudav1(A,x,y,n)
+    implicit none
+    integer(mINT), intent(in) :: n
+    real(kind=8), intent(in), device :: A(n*n)
+    real(kind=8), intent(in), device :: x(n)
+    real(kind=8), intent(out), device :: y(n)
+    type(dim3) :: blocks, threads
+    integer :: shmem ! shared memory in bytes
+    
+    blocks = dim3(2*80,1,1)
+    threads = dim3(512,1,1)
+    shmem = 0
+
+    call matvec_kernelv1<<<blocks,threads,shmem,0 >>>(A,x,y,n)
+  end subroutine matvec_cudav1
+    
+  attributes(global) subroutine matvec_kernelv1(A,x,y,n)
+    implicit none
+    integer(mINT), value, intent(in) :: n
+    real(kind=8), intent(in), device :: A(n*n)
+    real(kind=8), intent(in), device :: x(n)
+    real(kind=8), intent(out), device :: y(n)
+    
+    ! local variables
+    integer :: i, j
+    
+    ! simple parallel version of matrix vector multiplication. 
+    ! y = A*x
+    do j = threadIdx%x + blockDim%x*(blockIdx%x-1), n, gridDim%x*blockDim%x ! rows
+       y(j) = 0.0d+0
+       do i = 1, n ! columns
+          y(j) = y(j) + A(i + (j-1)*n) * x(i)
+       enddo
+    enddo    
+
+  end subroutine matvec_kernelv1
+
+
+
+  subroutine matvec_cudav2(A,x,y,n)
+    implicit none
+    integer(mINT), intent(in) :: n
+    real(kind=8), intent(in), device :: A(n*n)
+    real(kind=8), intent(in), device :: x(n)
+    real(kind=8), intent(out), device :: y(n)
+    type(dim3) :: blocks, threads
+    integer :: shmem ! shared memory in bytes
+    integer :: xthreads
+
+    xthreads = 32    
+
+    blocks = dim3(n/xthreads,1,1) ! TODO fix perfect division assumption
+    threads = dim3(xthreads,1,1)
+    shmem = (xthreads*32 + xthreads + 32)*8
+    print *, "v2 shmem : ", shmem
+    call matvec_kernelv2<<<blocks,threads,shmem,0 >>>(A,x,y,n)
+  end subroutine matvec_cudav2
+
+
+
+
+  attributes(global) subroutine matvec_kernelv2(A,x,y,n)
+    implicit none
+    integer(mINT), value, intent(in) :: n
+    real(kind=8), intent(in), device :: A(n*n)
+    real(kind=8), intent(in), device :: x(n)
+    real(kind=8), intent(out), device :: y(n)
+    
+    ! shared memory (local to blocks)
+    integer, parameter :: rrmax = 32
+    real(kind=8), shared :: A_s(blockDim%x,rrmax)
+    real(kind=8), shared :: x_s(blockDim%x)
+    real(kind=8), shared :: y_s(rrmax)
+    
+
+    ! local variables
+    integer :: i, j
+    integer :: rr, q
+
+
+    do q = threadIdx%x, n, blockDim%x
+       x_s(threadIdx%x) = x(q)
+       do rr=1, rrmax
+          A_s(threadIdx%x, rr) = A(q + (rr-1)*n + n*rrmax*(blockIdx%x-1))
+       enddo
+
+       call syncthreads() ! shared memory fence
+
+       do rr=threadIdx%x, rrmax, blockDim%x
+          y_s(rr) = 0.0d+0
+          do i = 1, blockDim%x ! columns
+             y_s(rr) = y_s(rr) + A_s(i,rr) * x_s(i)
+          enddo          
+          call syncthreads()
+          ! accumulate the shared memory y_s to global y
+          y(rr + rrmax*(blockIdx%x-1)) = y( rr + rrmax*(blockIdx%x-1)  ) + y_s(rr)
+       enddo
+    enddo
+       
+  end subroutine matvec_kernelv2
+
+
+
+
 
 
 end module matvec
