348f4c9cc671f321cd524f562bfaf10a37fa0f9c
David Appelhans <dappelh@us.ibm.com>
2021-03-17 12:21:27 -0400

    cpp serial version working

--- ex1-matvec/c/matVec.cu
+++ ex1-matvec/c/matVec.cu
@@ -9,8 +9,16 @@
  * Matrix multiplication (CUDA Kernel) on the device: C = A * B
  * wA is A's width and wB is B's width
  */
-void matvec_serial() {
-    printf("[hello in subroutine \n");
+void matvec_serial(double *A, double *x, double *y, int n) {
+    printf("hello in subroutine \n");
+
+    for(int j=0;j<n;++j){
+      y[j] = 0;
+      for(int i=0;i<n;++i){
+	y[j] = y[j] + A[i + n*j] * x[i];
+      }
+    }
+
 }
 
 
@@ -21,6 +29,31 @@ void matvec_serial() {
 int main(int argc, char **argv) {
     printf("[Matrix Vector Multiply Example CUDA] - Starting...\n");
 
-    matvec_serial();
+    int n;
+
+    if(argc < 2){
+      n = 20*1024;
+    }    
+    else{
+      n = atoi(argv[1]);
+    }
+    // Allocate memory:
+    double *A, *x, *y;
+    A = (double*) malloc(n*n*sizeof(*A));
+    x = (double*) malloc(n*n*sizeof(*x));
+    y = (double*) malloc(n*n*sizeof(*y));
+
+
+    for(int j=0;j<n;++j){
+      x[j] = 1;
+      for(int i=0;i<n;++i){
+	A[i + n*j]=1;
+      }
+    }
+
+    matvec_serial(A, x, y, n);
+
+    printf("y[2] = %f \n", y[2]);
+
 }
 
